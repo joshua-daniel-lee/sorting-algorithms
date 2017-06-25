b4c5941233ee13173a72df3d37ea4161fbf58755
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-06-24 19:15:43 -1000

    this is quick.js

---
+++ quick.js
@@ -0,0 +1,21 @@
+console.log('sanity check quick');
+
+function quickSort(numbers){
+  if (numbers.length < 2){
+    return numbers;
+  }
+
+  var pivot = numbers[0];
+  var left = [];
+  var right = [];
+
+  for (var i = 1; i < numbers.length; i++){
+    if (numbers[i] < pivot){
+      left.push(numbers[i]);
+    } else {
+      right.push(numbers[i]);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
+}
+console.log('this is the final answer of quick =', quickSort(numbers));
