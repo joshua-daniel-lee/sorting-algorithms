d1adf6fc2f84cf831776caa4279cdccb2d260f21
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-06-24 19:16:09 -1000

    this is selection.js

---
+++ selection.js
@@ -0,0 +1,25 @@
+//console.log('sanity check selection');
+
+
+function swap(numbers, i, j){
+  var temp = numbers[i];
+  numbers[i] = numbers[j];
+  numbers[j] = numbers[i];
+}
+
+function selectionSort(numbers){
+  for (var i = 0; i < numbers.length; i++){
+    var min = i;
+    console.log('this is min =', min);
+    for (var j = 1; j < numbers.length; j++){
+      if(numbers[j] < numbers[min]){
+        min = j;
+      }
+    }
+    if (i !== min){
+      swap(numbers, i, min);
+    }
+  }
+  return numbers;
+}
+console.log('this is the final answer of selection =', selectionSort(numbers));
