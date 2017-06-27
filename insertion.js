619e347a495336dde9f952dbd41cb63ed0f89878
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-06-26 17:07:31 -1000

    updating the insertion

--- insertion.js
+++ insertion.js
@@ -1,1 +1,15 @@
 //console.log('sanity check isertion');
+
+function insertionSort(numbers){
+  for (var i = 0; i < numbers.length; i++){
+    var temp = numbers[i];
+    var j = i - 1;
+    while (j >= 0 && numbers[j] > temp){
+      numbers[j + 1] = numbers[j];
+      j --;
+    }
+    numbers[j + 1] = temp;
+  }
+  return numbers;
+}
+console.log('this is the final answer of insertion =', insertionSort(numbers));
